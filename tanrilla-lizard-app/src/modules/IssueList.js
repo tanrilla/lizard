a00b0a6c8db8805a8a169f47bc65c488992459e0
Cesar <cesar.kina@gmail.com>
2019-03-23 08:52:01 -0500

    fix issue table

--- tanrilla-lizard-app/src/modules/IssueList.js
+++ tanrilla-lizard-app/src/modules/IssueList.js
@@ -5,57 +5,46 @@ import { getIssues, saveIssue, deleteIssue, getIssue, updateIssue, clearState } 
 
 class IssueList extends React.Component {
 
-  state = {
-    rows: [],
-  };
-
   componentDidMount () {
     this.props.getIssues();
-    console.log(this.props.issueList);
-    this.setState({ rows : this.props.issueList });
   }
 
   renderList() {
-    //console.log(this.state.rows);
-    return (
-      <Table>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Name</Table.HeaderCell>
-            <Table.HeaderCell>Registration Date</Table.HeaderCell>
-            <Table.HeaderCell>E-mail address</Table.HeaderCell>
-            <Table.HeaderCell>Premium Plan</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-
-        <Table.Body>
-          <Table.Row>
-            <Table.Cell>John Lilki</Table.Cell>
-            <Table.Cell>September 14, 2013</Table.Cell>
-            <Table.Cell>jhlilk22@yahoo.com</Table.Cell>
-            <Table.Cell>No</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>Jamie Harington</Table.Cell>
-            <Table.Cell>January 11, 2014</Table.Cell>
-            <Table.Cell>jamieharingonton@yahoo.com</Table.Cell>
-            <Table.Cell>Yes</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>Jill Lewis</Table.Cell>
-            <Table.Cell>May 11, 2014</Table.Cell>
-            <Table.Cell>jilsewris22@yahoo.com</Table.Cell>
-            <Table.Cell>Yes</Table.Cell>
-          </Table.Row>
-        </Table.Body>
-      </Table>
-    );
+    return this.props.issueList.map(issue => {
+      return (
+        <Table.Row key={issue.id}>
+          <Table.Cell>{issue.id}</Table.Cell>
+          <Table.Cell>{issue.summary}</Table.Cell>
+          <Table.Cell>{issue.status.name}</Table.Cell>
+          <Table.Cell>{issue.type.name}</Table.Cell>
+          <Table.Cell>{issue.project.name}</Table.Cell>
+          <Table.Cell>{issue.dueDate}</Table.Cell>
+          <Table.Cell>{issue.assignee.firstName}</Table.Cell>
+        </Table.Row>
+      );
+    });
   }
 
   render() {
     return (
         <div>
-            {this.renderList()}
+          <Table>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Id</Table.HeaderCell>
+                <Table.HeaderCell>Summary</Table.HeaderCell>
+                <Table.HeaderCell>Status</Table.HeaderCell>
+                <Table.HeaderCell>Type</Table.HeaderCell>
+                <Table.HeaderCell>Project</Table.HeaderCell>
+                <Table.HeaderCell>Due date</Table.HeaderCell>
+                <Table.HeaderCell>Asignee</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+              {this.renderList()}
+            </Table.Body>
+          </Table>
         </div>
     );
   }
